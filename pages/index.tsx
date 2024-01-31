3382333d05150fafedfdce11d2fd33a079510928
Gavin Stein <gste6115@uni.sydney.edu.au>
2024-01-31 21:34:48 +1000

    setting url debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ export default function Home() {
   useEffect(() => {
     if (typeof window !== "undefined") {
       redirectURI = process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL + "/callback" : 'http://localhost:3000/callback';
-      console.log(redirectURI);
+      console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
       const handleResize = () => {
         setIsMobile(window.innerWidth <= 768);
         console.log(window.innerWidth);
