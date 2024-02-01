e09c087f2d18ec641b85ba902029d30d3331314c
Gavin Stein <gste6115@uni.sydney.edu.au>
2024-02-01 11:42:27 +1000

    adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import {NextUIProvider} from "@nextui-org/react";
 import Header from "@/components/header";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <NextUIProvider>
     <Header />
     <Component {...pageProps} />
+    <Analytics />
   </NextUIProvider>
   )
 }
