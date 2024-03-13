1ad4b4eb31402bda04c1b8a84f8aea3484adf3a8
Gavin Stein <gste6115@uni.sydney.edu.au>
2024-03-13 11:29:59 +1100

    new bg

--- pages/api/init/group.tsx
+++ pages/api/init/group.tsx
@@ -17,7 +17,7 @@ export default async function handler(
     if (req.url?.includes("localhost")) {
         baseUrl = "http://localhost:3000";
     } else {
-        baseUrl = "https://s-blendid.vercel.app"
+        baseUrl = "https://splendit.au"
     }
     const payload = {
       method: 'POST',
